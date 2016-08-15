0144ff6d13b050918501ae87b69b2cd4e3cd5da5
Shane Xu <xusheng0711@gmail.com>
2016-08-15 15:38:16 +0800

    reduce memory usage

--- relay/retry.go
+++ relay/retry.go
@@ -45,18 +45,12 @@ func newRetryBuffer(size, batch int, max time.Duration, p poster) *retryBuffer {
 }
 
 func (r *retryBuffer) post(buf []byte, query string, auth string) (*responseData, error) {
-	pb := getBuf()
-	pb.Write(buf)
-	batch, err := r.list.add(pb.Bytes(), query, auth)
+	buf1 := make([]byte, len(buf))
+	copy(buf1, buf)
+	_, err := r.list.add(buf1, query, auth)
 	if err != nil {
-		putBuf(pb)
 		return nil, err
 	}
-
-	go func() {
-		batch.wg.Wait()
-		putBuf(pb)
-	}()
 
 	return &responseData{
 		StatusCode: 204,
@@ -78,7 +72,6 @@ func (r *retryBuffer) run() {
 			resp, err := r.p.post(buf.Bytes(), batch.query, batch.auth)
 			if err == nil && resp.StatusCode/100 != 5 {
 				batch.resp = resp
-				batch.wg.Done()
 				break
 			}
 
@@ -101,7 +94,6 @@ type batch struct {
 	size  int
 	full  bool
 
-	wg   sync.WaitGroup
 	resp *responseData
 
 	next *batch
@@ -113,7 +105,6 @@ func newBatch(buf []byte, query string, auth string) *batch {
 	b.size = len(buf)
 	b.query = query
 	b.auth = auth
-	b.wg.Add(1)
 	return b
 }
 
